b8cb91e90b7b80fa30bf83b2c79bb0e0f60fdf7d
Nasrullo Nurullaev <61620246+nasrullonurullaev@users.noreply.github.com>
2025-02-14 10:18:50 +0500

    Update check_comments.py

--- .github/workflows/check_comments.py
+++ .github/workflows/check_comments.py
@@ -8,7 +8,7 @@ EXCLUDED_FILES = {".json", ".p7s", ".cjs", ".po", ".license", ".xml", ".resx"}
 
 # Regex patterns
 COMMENT_REGEX = re.compile(r"(?://|#|<!--|/\*|\*).+")  # Matches comments in various languages
-NON_ASCII_REGEX = re.compile(r"^[\\p{Print}\\p{IsLatin}]*$"", re.UNICODE)  # Оставляет все печатные ASCII символы
+NON_ASCII_REGEX = re.compile(r"^[\p{Print}\p{Latin}]*$", re.UNICODE)  # Оставляет все печатные ASCII символы
 
 
 
